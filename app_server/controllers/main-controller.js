fc558b1524d09c906bd7da5b8776b9250d7ea401
Michael Mallari <michael@michaelmallari.com>
2016-01-07 07:47:00 -0500

    Temporarily hard coding JSON data

--- app_server/controllers/main-controller.js
+++ app_server/controllers/main-controller.js
@@ -1,3 +1,23 @@
 module.exports.home = function(req, res) {
-	res.render('index', { title: 'Cup of Wifi' });
+	res.render('index', {
+		title: 'Cup of Wifi',
+		apiData: {
+			dataset: {
+				columnNames: [
+					'Name',
+					'City',
+					'State',
+					'Mascot'
+				],
+				data: [
+					['Michigan State University', 'East Lansing', 'MI', 'Spartans'],
+					['University of Maryland', 'College Park', 'MD', 'Terrapins'],
+					['University of Michigan', 'Ann Arbor', 'MI', 'Wolverines'],
+					['Ohio State University', 'Columbus', 'OH', 'Buckeyes'],
+					['Penn State University', 'State College', 'PA', 'Nittany Lions'],
+					['Rutgers University', 'New Brunswick', 'NJ', 'Scarlet Knights']
+				]
+			}
+		}
+	});
 }
